e39d717af8a78f6a6b3347f3df91be2c00467052
cosmasnnabuike <cosmasnnabuike@gmail.com>
2024-12-08 13:49:40 +0000

    redux

---
+++ 0x09-react_redux_connectors_and_providers/task_9/dashboard/src/Notifications/Notifications.test.js
@@ -0,0 +1,203 @@
+import { shallow, mount } from "enzyme";
+import React from "react";
+import Notifications from "./Notifications";
+import { getLatestNotification } from "../utils/utils";
+import { StyleSheetTestUtils } from "aphrodite";
+import notificationsNormalizer from "../schema/notifications";
+import { Map, fromJS } from "immutable";
+import { getUnreadNotificationsByType } from "../selectors/notificationSelector";
+
+const NOTIFICATIONS = [
+  {
+    id: "5debd76480edafc8af244228",
+    author: {
+      id: "5debd764a7c57c7839d722e9",
+      name: {
+        first: "Poole",
+        last: "Sanders",
+      },
+      email: "poole.sanders@holberton.nz",
+      picture: "http://placehold.it/32x32",
+      age: 25,
+    },
+    context: {
+      guid: "2d8e40be-1c78-4de0-afc9-fcc147afd4d2",
+      isRead: true,
+      type: "urgent",
+      value:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.",
+    },
+  },
+  {
+    id: "5debd764507712e7a1307303",
+    author: {
+      id: "5debd7648ba8641ce0a34ea4",
+      name: {
+        first: "Norton",
+        last: "Grimes",
+      },
+      email: "norton.grimes@holberton.nz",
+      picture: "http://placehold.it/32x32",
+      age: 37,
+    },
+    context: {
+      guid: "cec84b7a-7be4-4af0-b833-f1485433f66e",
+      isRead: false,
+      type: "urgent",
+      value:
+        "ut labore et dolore magna aliqua. Dignissim convallis aenean et tortor at risus viverra adipiscing. Ac tortor dignissim convallis aenean et. ",
+    },
+  },
+  {
+    id: "5debd76444dd4dafea89d53b",
+    author: {
+      id: "5debd764a7c57c7839d722e9",
+      name: {
+        first: "Poole",
+        last: "Sanders",
+      },
+      email: "poole.sanders@holberton.nz",
+      picture: "http://placehold.it/32x32",
+      age: 25,
+    },
+    context: {
+      guid: "280913fe-38dd-4abd-8ab6-acdb4105f922",
+      isRead: false,
+      type: "urgent",
+      value:
+        "Non diam phasellus vestibulum lorem sed risus ultricies. Tellus mauris a diam maecenas sed",
+    },
+  },
+];
+
+describe("<Notifications />", () => {
+  let listNotifications;
+  let latestNotification;
+
+    //   console.log = jest.fn();
+
+    //   const instance = wrapper.instance();
+
+    //   const id = 5;
+
+    //   instance.markAsRead(id);
+
+    //   expect(console.log).toHaveBeenCalledWith(
+    //     `Notification ${id} has been marked as read`
+    //   );
+    //   jest.restoreAllMocks();
+    // });
+
+    // it("does not rerender when updating the props of the component with the same list", () => {
+    //   const listNotifications = [
+    //     { id: 1, type: "default", value: "New course available" },
+    //     { id: 2, type: "urgent", value: "New resume available" },
+    //   ];
+
+    //   const wrapper = shallow(
+    //     <Notifications displayDrawer listNotifications={listNotifications} />
+    //   );
+
+    //   const shouldComponentUpdate = jest.spyOn(
+    //     Notifications.prototype,
+    //     "shouldComponentUpdate"
+    //   );
+
+    //   wrapper.setProps({ listNotifications: listNotifications });
+
+    //   expect(shouldComponentUpdate).toHaveBeenCalled();
+    //   expect(shouldComponentUpdate).toHaveLastReturnedWith(false);
+
+    //   jest.restoreAllMocks();
+    // });
+
+    // it("does rerender when updating the props of the component with a longer list", () => {
+    //   let listNotifications = [
+    //     { id: 1, type: "default", value: "New course available" },
+    //     { id: 2, type: "urgent", value: "New resume available" },
+    //   ];
+
+    //   const listNotifications2 = [
+    //     { id: 1, type: "default", value: "New course available" },
+    //     { id: 2, type: "urgent", value: "New resume available" },
+    //     { id: 3, type: "urgent", html: { __html: latestNotification } },
+    //   ];
+
+    //   const wrapper = shallow(
+    //     <Notifications displayDrawer listNotifications={listNotifications} />
+    //   );
+
+    //   const shouldComponentUpdate = jest.spyOn(
+    //     Notifications.prototype,
+    //     "shouldComponentUpdate"
+    //   );
+
+    //   wrapper.setProps({ listNotifications: listNotifications2 });
+
+    //   expect(shouldComponentUpdate).toHaveBeenCalled();
+    //   expect(shouldComponentUpdate).toHaveLastReturnedWith(true);
+
+    //   jest.restoreAllMocks();
+    // });
+
+    it("verify that clicking on the menu item calls handleDisplayDrawer", () => {
+      const handleDisplayDrawer = jest.fn();
+      const handleHideDrawer = jest.fn();
+
+      const wrapper = shallow(
+        <Notifications
+          handleDisplayDrawer={handleDisplayDrawer}
+          handleHideDrawer={handleHideDrawer}
+        />
+      );
+
+      wrapper.find("#menuItem").simulate("click");
+
+      expect(handleDisplayDrawer).toHaveBeenCalled();
+      expect(handleHideDrawer).not.toHaveBeenCalled();
+
+      jest.restoreAllMocks();
+    });
+
+    it("verify that clicking on the button calls handleHideDrawer", () => {
+      const handleDisplayDrawer = jest.fn();
+      const handleHideDrawer = jest.fn();
+
+      const wrapper = shallow(
+        <Notifications
+          displayDrawer
+          handleDisplayDrawer={handleDisplayDrawer}
+          handleHideDrawer={handleHideDrawer}
+        />
+      );
+
+      wrapper.find("#closeNotifications").simulate("click");
+
+      expect(handleDisplayDrawer).not.toHaveBeenCalled();
+      expect(handleHideDrawer).toHaveBeenCalled();
+
+      jest.restoreAllMocks();
+    });
+
+    it("verify that clicking on the menu item calls handleDisplayDrawer", () => {
+      const setNotificationFilter = jest.fn();
+
+      const wrapper = shallow(
+        <Notifications
+          setNotificationFilter={setNotificationFilter}
+          displayDrawer={true}
+        />
+      );
+
+      wrapper.find("#buttonFilterUrgent").simulate("click");
+
+      expect(setNotificationFilter).toHaveBeenNthCalledWith(1, "URGENT");
+
+      wrapper.find("#buttonFilterDefault").simulate("click");
+
+      expect(setNotificationFilter).toHaveBeenNthCalledWith(2, "DEFAULT");
+
+      jest.restoreAllMocks();
+    });
+  });
+});
